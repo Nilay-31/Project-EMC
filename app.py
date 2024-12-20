e3c840ab8274c85e71440ffa3d19d55a3f4224db
Nilay-31 <nilaypatkar311@gmail.com>
2024-12-20 17:09:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import pandas as pd
 import pickle
 
 # Load the saved model
-#model_path = 'Project-EMC/email_open_prediction_model.pkl'
+model_path = 'Project-EMC/email_open_prediction_model.pkl'
 with open("email_open_prediction_model.pkl", 'rb') as model_file:
    model = pickle.load(model_file)
     
